55a611deb1aa6d96fdb76885826ed1fd32c050ae
volti <volticks@gmail.com>
2024-12-05 17:23:01 +0000

    Make clock puzzle clearer

--- public/javascripts/combo/clocks.js
+++ public/javascripts/combo/clocks.js
@@ -1,13 +1,18 @@
 const code = [7,3,5,5];
 // Player 1 gets idxs 1 and 3
 const idxs_plr1 = [0, 2];
-const idxs_plr2 = [0, 2];
+const idxs_plr2 = [1, 3];
 
 // Clock configuration, hour, minute
-const clock1Config = [0, 90*3];
+/*const clock1Config = [0, 90*3];
 const clock2Config = [90*2, 90];
 const clock3Config = [90, 0];
-const clock4Config = [90*3, 90*2];
+const clock4Config = [90*3, 90*2];*/
+
+const clock4Config = [45, (90*3) + 45];
+const clock3Config = [(90*2)+45, 90+45];
+const clock2Config = [90+45, 45];
+const clock1Config = [(90*3) + 45, (90*2) + 45];
 
 const configs = [clock1Config, clock2Config, clock3Config, clock4Config];
 
@@ -26,6 +31,12 @@ function setupClocks(){
 
     clockHandMin[0].style.transform = 'rotate('+clock1conf[1].toString()+'deg)';
     clockHandMin[1].style.transform = 'rotate('+clock2conf[1].toString()+'deg)';
+
+    const clockNum1 = document.getElementById("clockNums1");
+    const clockNum2 = document.getElementById("clockNums2");
+    clockNum1.textContent = "I".repeat(currentPlr[0]+1);
+    clockNum2.textContent = "I".repeat(currentPlr[1]+1);
+
 
 }
 
