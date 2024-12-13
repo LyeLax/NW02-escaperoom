75512f4cd4425077e17fd42981f3e3c3f7d57e83
volti <volticks@gmail.com>
2024-12-13 18:44:44 +0000

    Hey louis. Im now just using a text file instead of sending stuff to both routes like Nate suggested at the meeting.

--- routes/comboCheckerRouting.js
+++ routes/comboCheckerRouting.js
@@ -1,7 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('node:fs');
 
 var puzzleCompleted = 0;
+
+
+const fname = "public/comboComp.txt";
+
+function loadPuzzleCompVal(){
+  try{
+    const cont = fs.readFileSync(fname, 'utf8');
+
+    const num = Number.parseInt(cont);
+    if (Number.isNaN(num)){
+      console.log("Got NaN parsing value in combo completion: " + cont);
+      return num;
+    }
+
+    return num;
+  } catch(err){
+    console.log("Error reading combo comp file: ", err);
+    return "";
+  }
+}
+
+function setPuzzleCompVal(val){
+
+  if (val < 0 || val > 2) return;
+
+  fs.writeFile(fname, val.toString(), err => {
+    if (err)
+      console.log("Got err writing in combo completion: " + err);
+  });
+}
 
 console.log("Attempting routing - checker");
 router.post("/setPuzzleCompleted", (req, res) => {
@@ -13,7 +44,10 @@ router.post("/setPuzzleCompleted", (req, res) => {
       res.send("What?");
       return;
     }
-    puzzleCompleted = completed % 2;
+
+    setPuzzleCompVal(completed % 3);
+    puzzleCompleted = completed % 3;
+
     console.log("Puzzle completed val: " + puzzleCompleted);
     res.send("Dun");
 });
@@ -25,7 +59,9 @@ router.get("/getPuzzleCompleted", (req, res) => {
       'Content-Length': '1',
       // extra headers here
     });
-    res.send(puzzleCompleted.toString());
+
+    const val = loadPuzzleCompVal();
+    res.send(val.toString());
 });
 
 console.log("Attempting routing - checker, complete");
