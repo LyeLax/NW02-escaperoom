8d83113cae5150fd85108c5ca1b3ef98e994c2ca
JakeHowza <w21023103@northumbria.ac.uk>
2024-12-16 14:20:10 +0000

    Created P2 room all that's left is checking if correct and puzzle completion

---
+++ public/javascripts/mapPuzzle/mapScriptsP2.js
@@ -0,0 +1,20 @@
+
+
+function displayPoster(){
+    document.getElementById("posterViewP2").className = "show";
+    console.log(document.getElementById("posterView").className);
+}
+
+function hidePoster(){
+    document.getElementById("posterViewP2").className = "hide";
+}
+
+function displayMap(){
+    document.getElementById("mapView").className = "show";
+    console.log(document.getElementById("posterView").className);
+}
+
+function hideMap(){
+    document.getElementById("mapView").className = "hide";
+}
+
